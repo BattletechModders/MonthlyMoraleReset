442eef0c1fefa1b7835fec10821a0e5130ede023
Zathoth <Zathoth@outlook.com>
2018-05-23 20:54:57 +0200

    first push

--- MonthlyMoraleReset/Properties/AssemblyInfo.cs
+++ MonthlyMoraleReset/Properties/AssemblyInfo.cs
@@ -1,18 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
 [assembly: AssemblyTitle("MonthlyMoraleReset")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("ResetMorale Mod for Battletech by Harebrained Schemes")]
 [assembly: AssemblyProduct("MonthlyMoraleReset")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
 // aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
